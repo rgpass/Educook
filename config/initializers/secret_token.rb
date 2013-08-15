cd5546b6020f348bd33f5d6bd8343d59330cf440
Gerry Pass <rgpass@gmail.com>
2013-08-15 15:37:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educook::Application.config.secret_token = '4e95414f63985e09183f2e7b0743a197e8fc8843570707273f794da7bdc82365085a8cf279ea9d62fb7afa2c4975d6ebc83ecd0f8df39ac1816028dba62a82c3'
